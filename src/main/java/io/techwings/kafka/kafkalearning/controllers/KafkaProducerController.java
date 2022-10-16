5c32954da7281a6dadc41907d530c8a276a6d989
Marco Tedone <marco@techwings.io>
2022-10-16 18:45:05 +0100

    Added integration test

--- src/main/java/io/techwings/kafka/kafkalearning/controllers/KafkaProducerController.java
+++ src/main/java/io/techwings/kafka/kafkalearning/controllers/KafkaProducerController.java
@@ -1,10 +1,13 @@
 package io.techwings.kafka.kafkalearning.controllers;
 
+import java.net.URI;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriBuilder;
 
 import io.techwings.kafka.kafkalearning.dto.Party;
 import io.techwings.kafka.kafkalearning.services.ProducerService;
@@ -22,6 +25,6 @@ public class KafkaProducerController {
     @PostMapping("/publish")
     public ResponseEntity<String> publish(@RequestBody Party party) {
         kafkaProducer.sendMessage(party);
-        return ResponseEntity.ok("Message sent to kafka topic");
+        return ResponseEntity.ok("Message sent to /kafka/topic/first_topic");
     }
 }
---
+++ src/test/java/io/techwings/kafka/kafkalearning/KafkaLearningApplicationTestsIntegrationTest.java
@@ -0,0 +1,73 @@
+package io.techwings.kafka.kafkalearning;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.annotation.DirtiesContext;
+
+import io.techwings.kafka.kafkalearning.controllers.KafkaProducerController;
+import io.techwings.kafka.kafkalearning.dto.Party;
+import io.techwings.kafka.kafkalearning.services.ConsumerService;
+import io.techwings.kafka.kafkalearning.services.ProducerService;
+
+/**
+ * Assumes a local Kafka instance running on localhost:9092 with an existing
+ * topic of first_topic.
+ * 
+ * You can run the test by using Postman and providing the following JSON as
+ * payload for a POST:
+ * <code>
+ * {
+ * "firstName": "Marco",
+ * "lastName": "Tedone"
+ * }
+ * </code>
+ * 
+ */
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+class KafkaLearningApplicationTestsIntegrationTest {
+
+	private static final Logger LOG = LoggerFactory.getLogger(KafkaLearningApplicationTestsIntegrationTest.class);
+
+	@Autowired
+	private KafkaProducerController controller;
+
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@Test
+	void contextLoads() {
+		assertThat(controller).isNotNull();
+	}
+
+	/**
+	 * @throws Exception
+	 */
+	@Test
+	void ensureSendMessageToKafkaWorks() throws Exception {
+		Party party = new Party("Unit Test", "Spring Boot");
+		final ResponseEntity<String> response = this.restTemplate.postForEntity("/api/v1/kafka/publish", controller,
+				String.class, party);
+		assertNotNull(response);
+		LOG.info("Response status code is: {}", response.getStatusCode());
+		LOG.info("Response body is: {}", response.getBody().toString());
+	}
+
+}
